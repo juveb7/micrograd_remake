e8512eccf9b5a87e092e185ed1c87f00776752de
Juvenal F Barajas <juveb0111@gmail.com>
2023-07-10 14:15:50 -0700

    pushing local work

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # micrograd_remake
 Tiny autograd engine.
+
+### Non-efficient way of training NNs
+### Useful for understanding the complexity of NN training
---
+++ layer_class.py
@@ -0,0 +1,59 @@
+import random
+from value_class import Value
+
+class Neuron:
+    def __init__(self, nin):
+        self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
+        self.b = Value(random.uniform(-1,1))
+
+    
+    def __call__(self, x):
+        # w * x + b
+        # zip: takes 2 iterators & creates a new iterator that iterates over the tuples of the corresponding entries
+        activation = sum((wi*xi for wi, xi in zip(self.w, x)), self.b)
+        out = activation.tanh()
+        return out
+    
+
+    def parameters(self):
+        return self.w + [self.b]
+
+class Layer:
+    # nouts: number of outputs -> how many neurons do you want?
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+
+    
+    def __call__(self, x):
+        outs = [n(x) for n in self.neurons]
+        return outs[0] if len(outs) == 1 else outs
+    
+
+    def parameters(self):
+
+        return [p for neuron in self.neurons for p in neuron.parameters()]
+        # params = []
+
+        # for neuron in self.neurons:
+        #     ps = neuron.parameters()
+        #     params.extend(ps)
+        # return params
+
+
+class MLP:
+    
+    # nouts: list of nouts
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+
+    
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        
+        return x
+    
+
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
